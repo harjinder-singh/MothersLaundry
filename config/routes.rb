f56726a7db2a0beb47eb5d161da734e0a165429c
Harjinder <harjinderfcsi@gmail.com>
2016-11-24 22:59:10 +0530

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations" }
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+    get "register" => "devise/registrations#new"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
