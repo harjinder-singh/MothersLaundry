a753b4f286171c36f8dfe44942e8f29467449693
Harjinder <harjinderfcsi@gmail.com>
2016-11-26 22:59:45 +0530

    Added Quick Order

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  resources :orders
+  resources :orders do
+    collection do
+      post 'quick_order'
+    end
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
