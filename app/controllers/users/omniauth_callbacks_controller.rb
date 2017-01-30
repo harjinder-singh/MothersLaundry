c235e41b32d8ba451ea319629acfe7e02f82b1c7
Harjinder <harjinderfcsi@gmail.com>
2017-01-30 23:26:52 +0530

    Redirected to verification path after omniauth call

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
+        sign_in @user
+        redirect_to verification_path
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
