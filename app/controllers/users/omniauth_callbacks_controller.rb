f1dcaf632f1c90336ffdaad9e3290f308da77c38
Harjinder <harjinderfcsi@gmail.com>
2017-01-30 23:37:38 +0530

    minor change

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in @user
-        redirect_to verification_path
+        redirect_to @user.phone_no.present? ? root_path : verification_path
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
